57621a9a6325b474c13e52d78d68558c9710cc5d
DeadLine <93661423+Comotcaiten@users.noreply.github.com>
2025-05-06 15:19:38 +0700

    update lại việc quản lý bomb

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -11,6 +11,7 @@ public class Bomb : MonoBehaviour
 
     public GameObject flameStartPrefab;
     public GameObject flameMiddlePrefab;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -23,38 +24,10 @@ public class Bomb : MonoBehaviour
     {
         yield return new WaitForSeconds(2.0f);
 
-        Vector3Int center = tileDestruction.WorldToCell(transform.position);
-        int range = 2; // bán kính nổ (2 ô mỗi hướng)
+        Debug.Log("DestroyAfter");
 
-        // Phá tile ở vị trí tâm
-        tileDestruction.SetTile(center, null);
-        Debug.Log("Destroyed tile at: " + center);
-
-        // Hướng trái và phải (x)
-        for (int dx = 1; dx <= range; dx++)
-        {
-            Vector3Int posRight = new Vector3Int(center.x + dx, center.y, 0);
-            Vector3Int posLeft = new Vector3Int(center.x - dx, center.y, 0);
-
-            tileDestruction.SetTile(posRight, null);
-            tileDestruction.SetTile(posLeft, null);
-
-            Debug.Log("Destroyed tile at: " + posRight);
-            Debug.Log("Destroyed tile at: " + posLeft);
-        }
-
-        // Hướng lên và xuống (y)
-        for (int dy = 1; dy <= range; dy++)
-        {
-            Vector3Int posUp = new Vector3Int(center.x, center.y + dy, 0);
-            Vector3Int posDown = new Vector3Int(center.x, center.y - dy, 0);
-
-            tileDestruction.SetTile(posUp, null);
-            tileDestruction.SetTile(posDown, null);
-
-            Debug.Log("Destroyed tile at: " + posUp);
-            Debug.Log("Destroyed tile at: " + posDown);
-        }
+        // Vector3Int center = tileDestruction.WorldToCell(transform.position);
+        // int range = 2; // bán kính nổ (2 ô mỗi hướng)
 
         Destroy(gameObject);
 
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -6,35 +5,20 @@ public class PlayerController : MonoBehaviour
     public float speed = 5.0f;
     public float horizontalInput;
     public float verticalInput;
-    public GameObject bomPrefab;
     private Rigidbody2D playerRb;
 
-    int bombRemaining;
-    int bombAmount = 1;
-    float bombFuseTime;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         playerRb = GetComponent<Rigidbody2D>();
 
-        bombRemaining = bombAmount;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (bombRemaining > 0 && Input.GetKeyDown(KeyCode.Space))
-        {
-            PlaceBomb();
-        }
     }
 
     void FixedUpdate()
     {
         MoveInput();
     }
-
 
     void MoveInput()
     {
@@ -44,23 +28,8 @@ public class PlayerController : MonoBehaviour
         playerRb.linearVelocity =  ((Vector2.up * verticalInput) + (Vector2.right * horizontalInput)) * speed;
     }
 
-
-    IEnumerator PlaceBomb() 
-    {
-
-        Vector2 position = new Vector2(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.y));
-
-        Instantiate(bomPrefab, position, bomPrefab.transform.rotation);
-
-        bombRemaining--;
-
-        yield return new WaitForSeconds(bombFuseTime);
-
-        bombRemaining++;
-    }
-
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        Debug.Log($"name: {collision.gameObject.name}");
-    }
+    // void OnCollisionEnter2D(Collision2D collision)
+    // {
+    //     Debug.Log($"name: {collision.gameObject.name}");
+    // }
 }
