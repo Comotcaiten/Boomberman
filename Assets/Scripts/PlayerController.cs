7305730bf5743344ba8638aacf01cfe524c5eece
DeadLine <93661423+Comotcaiten@users.noreply.github.com>
2025-05-20 17:30:47 +0700

    test an bat cu dau cung dat bomb thay vi button

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -63,7 +63,7 @@ public class PlayerController : MonoBehaviour
         animator.SetFloat("speed", (moveInput.x * moveInput.x + moveInput.y * moveInput.y));
 
         // moveInput.Normalize(); // Đưa về vector đơn vị
-        // playerRb.linearVelocity = ((Vector2.up * moveInput.y) + (Vector2.right * moveInput.x)) * moveSpeed;
+        playerRb.linearVelocity = ((Vector2.up * moveInput.y) + (Vector2.right * moveInput.x)) * moveSpeed; 
 
         // Vector2 move = moveInput * moveSpeed * Time.deltaTime;
         // transform.Translate(move);
