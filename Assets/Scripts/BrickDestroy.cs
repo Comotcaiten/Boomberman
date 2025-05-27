ee813bc885c96e7f8c07dc31b5a739b0b3992b0b
Trọng Nhân <trongnhan10a11@gmail.com>
2025-05-27 10:04:06 +0700

    chỉnh sửa UI Menu cho đầy màu sắc và sống động hơn

--- Assets/Scripts/BrickDestroy.cs
+++ Assets/Scripts/BrickDestroy.cs
@@ -1,22 +1,23 @@
-    using System.Collections;
-    using UnityEngine;
-    public class BrickDestroy : MonoBehaviour
+using System.Collections;
+using UnityEngine;
+public class BrickDestroy : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
-        {
-            StartCoroutine(DestroyAfter());
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-            
-        }
-
-        IEnumerator DestroyAfter() {
-            yield return new WaitForSeconds(0.75f);
-
-            Destroy(gameObject);
-        }
+        StartCoroutine(DestroyAfter());
     }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator DestroyAfter()
+    {
+        yield return new WaitForSeconds(0.75f);
+
+        Destroy(gameObject);
+    }
+}
